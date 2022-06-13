d685451636cfab1982677f8198a6d362df427d25
Paulo de Souza <paulocommander13@gmail.com>
2022-06-13 13:01:25 -0400

    PyGame Introduction!
    Rendering images and text. Opening a basic screen.

---
+++ pygame/intro.py
@@ -0,0 +1,26 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('My Game', False, 'Green')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 150))
+
+    pygame.display.update()
+    clock.tick(60)
