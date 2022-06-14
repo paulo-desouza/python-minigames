b9783a1078ccb0f29b99847ede96d5c0904666ad
Paulo de Souza <paulocommander13@gmail.com>
2022-06-14 12:15:41 -0400

    Adding movement, and adding Rectangles for better positioning.

--- pygame/intro.py
+++ pygame/intro.py
@@ -8,9 +8,15 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
-text_surface = test_font.render('My Game', False, 'Green')
+sky_surf = pygame.image.load('graphics/Sky.png').convert()
+ground_surf = pygame.image.load('graphics/ground.png').convert()
+text_surf = test_font.render('My Game', False, 'Black')
+
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom = (600, 300))
+
+player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -18,9 +24,16 @@ while True:
             pygame.quit()
             exit()
     
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 150))
+    screen.blit(sky_surf, (0, 0))
+    screen.blit(ground_surf, (0, 300))
+    screen.blit(text_surf, (300, 150))
+    if snail_rect.x > -100:
+        snail_rect.left -= 4
+    else:
+        snail_rect.x = 900
+    screen.blit(snail_surf, snail_rect)
+    player_rect.left += 1
+    screen.blit(player_surf, player_rect)
 
     pygame.display.update()
     clock.tick(60)
